2d9013bcf7aaca50c2bbcd0557b48003f6247893
Micha? K <Micha? K.@localhost>
2014-01-06 23:05:12 +0100

    All tests passing

--- include/puzzleboard.h
+++ include/puzzleboard.h
@@ -43,39 +43,72 @@ private:
     std::map < std::shared_ptr<SourceObj>, std::shared_ptr<DestObj> > mappedPointers;
 };
 
-typedef PointerToPointerMapper<Position2D,Puzzle> PositionToPuzzleMapper;
+typedef PointerToPointerMapper<Position2D,NumericPuzzle> PositionToNumericPuzzleMapper;
+
 
 template <class T>
-class PuzzleBoard : public Dimension2D
+class PuzzleBoardContainer : public Dimension2D
 {
 public:
-    PuzzleBoard(const Dimension2D &d):
+    PuzzleBoardContainer(const Dimension2D &d):
         Dimension2D(d)
     {
 
     }
 
-    virtual ~PuzzleBoard(){}
+    void setPuzzle(std::shared_ptr<Position2D> pos, std::shared_ptr<T> puzzle)
+    {
+        this->puzzles.mapPointerToPointer(pos,puzzle);
+    }
 
+    std::shared_ptr<T> getPuzzle(std::shared_ptr<Position2D> pos)
+    {
+        return puzzles.getPointerFromPointer(pos);
+    }
+
+//    void setPuzzlesTo(T value){
+//           for (int y=0; y<this->verticalSize; y++)
+//           {
+//               for (int x=0; x<this->horizontalSize; x++)
+//               {
+//                   this->setPuzzle(std::shared_ptr<Position2D>(new Position2D(x,y)),std::shared_ptr<T>(new T(x,y)));
+//                   PointState<T> p(x,y,value);
+//                   setState(p);
+//               }
+//           }
+//       }
+
+    virtual ~PuzzleBoardContainer(){}
+
+protected:
+    PointerToPointerMapper<Position2D,T> puzzles;
 };
 
-typedef PuzzleBoard<int> NumericPuzzleBoard;
 
-//template <class T>
-//class PuzzleBoardState : public PointStateContainer<T>
-//{
-//public:
-//    PuzzleBoardState(const Dimension2D &d):
-//        PointStateContainer<T>(d)
-//    {
+typedef PuzzleBoardContainer<int> NumericPuzzleBoardContainer;
 
-//    }
 
-//    T getPuzzleAt
+template <class T>
+class PuzzleBoard : public Dimension2D
+{
+public:
+     PuzzleBoard(const Dimension2D &d, const PuzzleBoardContainer<T> &container):
+         Dimension2D(d), puzzleContainer(container)
+     {
 
-//    virtual ~PuzzleBoardState(){}
-//};
+     }
 
-//typedef  PuzzleBoardState<int> NumericPuzzleBoardState;
+     std::shared_ptr<T> getPuzzle(std::shared_ptr<Position2D> pos)
+     {
+         return puzzleContainer.getPuzzle(pos);
+     }
+
+     virtual ~PuzzleBoard(){}
+
+protected:
+     PuzzleBoardContainer<T> puzzleContainer;
+};
+
+typedef  PuzzleBoard<int> NumericPuzzleBoard;
 
 #endif // PUZZLEBOARD_H
---
+++ include/puzzleboardmanager.h
@@ -0,0 +1,10 @@
+#ifndef PUZZLEBOARDMANAGER_H
+#define PUZZLEBOARDMANAGER_H
+
+class PuzzleBoardManager
+{
+public:
+    PuzzleBoardManager();
+};
+
+#endif // PUZZLEBOARDMANAGER_H
