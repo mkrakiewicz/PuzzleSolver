714a8a60824b09d5ffbb247d0f5fe74e381adddc
Micha? K. <Micha? K.@localhost>
2014-01-08 00:27:01 +0100

    ddd

--- src/puzzleboardsmanager.cpp
+++ src/puzzleboardsmanager.cpp
@@ -1,5 +1,6 @@
 #include "puzzleboardsmanager.h"
 
+using namespace std;
 
 NumericPuzzleBoardsManager::NumericPuzzleBoardsManager(const Dimension2D &d):
     PuzzleBoardsManager<NumericPuzzle>(d)
@@ -8,13 +9,17 @@ NumericPuzzleBoardsManager::NumericPuzzleBoardsManager(const Dimension2D &d):
 
 NumericPuzzleBoard NumericPuzzleBoardsManager::getEmptyBoard()
 {
-    const Dimension2D dim(sizeX,sizeY);
-    NumericPuzzleBoardContainer c(dim);
 
-    for (u_int x = 0; x<sizeX; x++ ){
-            for (u_int y = 0; y<sizeY; y++ ){
+    NumericPuzzleBoardContainer c(getDimensions());
+
+    for (u_int x = 0; x<c.horizontalSize; x++ )
+    {
+            for (u_int y = 0; y<c.verticalSize; y++ )
+            {
                 c.setPuzzle(shared_ptr<Position2D> (points[Position2D(x,y)]), shared_ptr<NumericPuzzle> (new NumericPuzzle(0)));
             }
     }
+
+    return NumericPuzzleBoard(c.getDimensions(),c);
 
 }
