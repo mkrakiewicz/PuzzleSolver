496e090dbcd8ad2ce8968b28f3fd8bf16591d963
Micha? K. <Micha? K.@localhost>
2014-01-08 21:48:09 +0100

    from work

---
+++ src/puzzlesolver.cpp
@@ -0,0 +1,34 @@
+#include "puzzlesolver.h"
+#include "puzzleboard.h"
+#include "exceptions.h"
+
+using namespace boards;
+
+PuzzleSolver::PuzzleSolver()
+    :boardToSolve(0),result(0)
+{
+}
+
+void PuzzleSolver::solve()
+{
+    if (!boardToSolve)
+        return;
+
+}
+
+void PuzzleSolver::setBoard(std::shared_ptr<PuzzleBoard> b) throw()
+{
+    if (b == 0)
+        throw new Exception("Trying to set NULL Board");
+    boardToSolve = b;
+}
+
+std::shared_ptr<PuzzleBoard> PuzzleSolver::getResult()
+{
+    if (result == 0)
+    {
+        std::shared_ptr<PuzzleBoard> p(new EmptyBoard(boardToSolve->getDimensions()));
+        return p;
+    }
+    return result;
+}
