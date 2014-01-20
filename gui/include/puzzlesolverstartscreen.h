daa73e3951bb7b98336c68c89929ebd8fbd6eef6
Micha? K <Micha? K.@localhost>
2014-01-20 01:10:35 +0100

    ghj

--- gui/include/puzzlesolverstartscreen.h
+++ gui/include/puzzlesolverstartscreen.h
@@ -2,9 +2,14 @@
 #define PUZZLESOLVERSTARTSCREEN_H
 
 #include <QMainWindow>
+#include <memory>
 
 namespace Ui {
-class PuzzleSolverStartScreen;
+    class PuzzleSolverStartScreen;
+}
+
+namespace board {
+    class Dimension2D;
 }
 
 class PuzzleSolverStartScreen : public QMainWindow
@@ -15,10 +20,15 @@ public:
     explicit PuzzleSolverStartScreen(QWidget *parent = 0);
     ~PuzzleSolverStartScreen();
     
+
+protected:
+    std::shared_ptr<board::Dimension2D> boardSize;
 private slots:
     void on_horizontalBoardSizeSlider_valueChanged(int value);
 
     void on_verticalBoardSizeSlider_valueChanged(int value);
+
+    void on_pushButton_clicked();
 
 private:
     Ui::PuzzleSolverStartScreen *ui;
--- gui/src/puzzlesolverstartscreen.cpp
+++ gui/src/puzzlesolverstartscreen.cpp
@@ -1,8 +1,15 @@
+#include "stdafx.h"
+
+#include <QThread>
 #include "puzzlesolverstartscreen.h"
 #include "ui_puzzlesolverstartscreen.h"
+#include "puzzleboard.h"
+
+using namespace board;
 
 PuzzleSolverStartScreen::PuzzleSolverStartScreen(QWidget *parent) :
     QMainWindow(parent),
+    boardSize(new Dimension2D),
     ui(new Ui::PuzzleSolverStartScreen)
 {
     ui->setupUi(this);
@@ -19,6 +26,9 @@ void PuzzleSolverStartScreen::on_horizontalBoardSizeSlider_valueChanged(int valu
     auto y = ui->verticalBoardSizeSlider->value();
     ui->labelBoardSize->setText(QString::number(x) + " x " + QString::number(y));
 
+    boardSize->horizontalSize = x;
+
+
 }
 
 void PuzzleSolverStartScreen::on_verticalBoardSizeSlider_valueChanged(int value)
@@ -26,4 +36,18 @@ void PuzzleSolverStartScreen::on_verticalBoardSizeSlider_valueChanged(int value)
     auto x = ui->horizontalBoardSizeSlider->value();
     auto y = value;
     ui->labelBoardSize->setText(QString::number(x) + " x " + QString::number(y));
+
+    boardSize->verticalSize = y;
+
+}
+
+void PuzzleSolverStartScreen::on_pushButton_clicked()
+{
+    for (int i =0; i<200; i++)
+    {
+        auto g = ui->dial->geometry();
+        g.translate(1,0);
+        ui->dial->setGeometry(g);
+        QThread::msleep (20);
+    }
 }
