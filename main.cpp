17e0ffdb25fcc71ed2e33a070396565e8c534980
Micha? K <Micha? K.@localhost>
2014-01-29 04:19:16 +0100

    yesssssssss

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "puzzlesolverstartscreen.h"
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
+    QTranslator translator;
+    translator.load("../lang");
+    a.installTranslator(&translator);
     PuzzleSolverStartScreen w;
     w.setFixedSize(w.size());
     w.show();
