4081b8c068cbae9d7006b02375d3f97238917abf
Micha? K. <Micha? K.@localhost>
2014-01-29 04:19:16 +0100

    yesssssssss

--- lang.ts
+++ lang.ts
@@ -1,30 +1,243 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="pl_PL" sourcelanguage="en">
 <context>
-    <name>PSWindow</name>
+    <name>AboutDialog</name>
     <message>
-        <source>PSWindow</source>
-        <translation type="unfinished"></translation>
+        <location filename="../../source/gui/aboutdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Okno</translation>
     </message>
     <message>
-        <source>Opcje</source>
-        <translation type="unfinished"></translation>
+        <location filename="../../source/gui/aboutdialog.ui" line="20"/>
+        <source>About</source>
+        <translation type="unfinished">O programie...</translation>
     </message>
     <message>
-        <source>Pomoc</source>
-        <translation type="unfinished"></translation>
+        <location filename="../../source/gui/aboutdialog.ui" line="44"/>
+        <source>Author:</source>
+        <translation>Autor:</translation>
     </message>
     <message utf8="true">
-        <source>Zmień obrazek tła...</source>
+        <location filename="../../source/gui/aboutdialog.ui" line="51"/>
+        <source>Michał Krakiewicz</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/aboutdialog.ui" line="58"/>
+        <source>Group</source>
+        <translation>Grupa</translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/aboutdialog.ui" line="65"/>
+        <source>331 IZZ</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/aboutdialog.ui" line="72"/>
+        <source>Academic Year</source>
+        <translation>Rok akademicki</translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/aboutdialog.ui" line="79"/>
+        <source>2013/2014</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ErrorDialog</name>
+    <message>
+        <location filename="../../source/gui/errordialog.ui" line="14"/>
+        <source>Dialog</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>O programie...</source>
+        <location filename="../../source/gui/errordialog.ui" line="20"/>
+        <source>Information</source>
+        <translation type="unfinished">Informacja</translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/errordialog.ui" line="26"/>
+        <source>Message</source>
+        <translation type="unfinished">Wiadomość
+</translation>
+    </message>
+</context>
+<context>
+    <name>PuzzleSolverStartScreen</name>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="23"/>
+        <source>Puzzle Solver</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
-        <source>Wyjdź</source>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="52"/>
+        <source>Set up Board</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="76"/>
+        <source>Welcome in PuzzleSolver</source>
+        <translation>Program Układanie Puzzli</translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="114"/>
+        <source>Please choose board size first:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="135"/>
+        <source>3 x 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="147"/>
+        <source>Horizontal size(X)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="173"/>
+        <source>Vertical size(Y)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="211"/>
+        <source>Create new board</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="239"/>
+        <source>Preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="256"/>
+        <source>Solve Puzzle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="264"/>
+        <source>Puzzle view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="322"/>
+        <source>By steps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="351"/>
+        <source>By percentage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="366"/>
+        <source>Shuffle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="387"/>
+        <source>Puzzle options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="405"/>
+        <source>Priority functions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="412"/>
+        <source>Hamming</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="422"/>
+        <source>Manhattan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="447"/>
+        <source>CheckBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="458"/>
+        <source>Show solution animation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="471"/>
+        <source>Solve</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="491"/>
+        <source>Solution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="497"/>
+        <source>Solution steps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="535"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="543"/>
+        <source>Solution statistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="550"/>
+        <source>Not solved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="557"/>
+        <source>Steps:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="1003"/>
+        <source>Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="1007"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="1011"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="1024"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="1034"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="1039"/>
+        <source>Polish</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="1044"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../source/gui/puzzlesolverstartscreen.ui" line="1049"/>
+        <source>Author</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
